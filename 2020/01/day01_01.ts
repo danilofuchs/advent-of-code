7e631a37cc041ac1d0700b8ad62849cb3a4c0d32
Danilo Campana Fuchs <danilo.fuchs@ebanx.com>
2020-12-01 21:28:08 -0300

    Day 1 - Part 1

---
+++ 2020/01/day01_01.ts
@@ -0,0 +1,19 @@
+const decoder = new TextDecoder("utf-8");
+const file = await Deno.readFile("./numbers.csv");
+const numbers = decoder
+  .decode(file)
+  .split("\n")
+  .map((str) => parseInt(str));
+
+const target = 2020;
+
+for (const number of numbers) {
+  const complement = numbers.find(
+    (complement) => number + complement === target
+  );
+  if (complement !== undefined) {
+    console.log(`${number} + ${complement} = ${target}`);
+    console.log(`${number} * ${complement} = ${number * complement}`);
+    break;
+  }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+## Advent of Code
+
+My [Advent of Code](https://adventofcode.com/) solutions:
+
+- [2020](/2020) - Written in [Deno](https://deno.land/)
