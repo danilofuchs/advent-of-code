fd26a27b8abbd712081bd4353ccffb80751a9edb
Danilo Campana Fuchs <danilo.fuchs@ebanx.com>
2020-12-14 21:20:34 -0300

    Day 4 - Part 1

---
+++ 2020/04/day04_01.ts
@@ -0,0 +1,29 @@
+const decoder = new TextDecoder("utf-8");
+const file = await Deno.readFile("./passports.txt");
+const decoded = decoder.decode(file);
+
+const requiredFields = [
+  "byr",
+  "iyr",
+  "eyr",
+  "hgt",
+  "hcl",
+  "ecl",
+  "pid",
+  // "cid" // optional
+];
+
+const validPassports = decoded
+  .split("\n\n")
+  .map((passportData) => passportData.split(/[\s]/g))
+  .map((fields) => fields.map((field) => field.split(":")[0]))
+  .map((fieldNames) =>
+    requiredFields.every((requiredField) => fieldNames.includes(requiredField))
+  );
+
+const validCount = validPassports.reduce(
+  (acc, curr) => (acc += curr ? 1 : 0),
+  0
+);
+
+console.log(validCount);
